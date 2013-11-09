ae505f60f9662aac33c7c3398634aa1ae474fed0
Scott Burton <scott.burton@hyfn.com>
2013-11-08 23:25:29 -0800

    session 2 docs

--- session_2/eval_wizard_1.rb
+++ session_2/eval_wizard_1.rb
@@ -4,15 +4,31 @@ class LameWizard
   end
 end
 
+# LameWizard is lame.
+LameWizard.new.lame_spell
+# => "This is lame"
+
+# EvalWizarrrd can make up any spell he wants.
+# EvalWizarrrd don't care.
 class EvalWizarrrd
+
+  # create a new Spell; if a block is passed,
+  # evaluate the block in the context of the 
+  # new Spell. 
   def spell name, &blk
     spell = Spell.new(name)
     spell.instance_eval(&blk) if block_given?
     spell
   end
 
+  # Use our #spell method to create a custom
+  # spell. Since the block's `self` is an instance
+  # of Spell, this is a baby DSL.
   def lightning_bolt(tgt)
     spell "Lighting Bolt!!! ZAPP!!!" do
+      # Blocks are closures! Even though this block's 
+      # `self` is a Spell, we can pass a +tgt+ in from
+      # the outside.
       target tgt
       damage 111
     end
@@ -32,6 +48,9 @@ class EvalWizarrrd
       @damage = dmg
     end
 
+    # I normally don't recommend using instance variables
+    # this way, but it's necessary if we want to use the
+    # method as a setter.
     def cast
       return "#{@name} Missed!" unless @target
       "Hit #{@target} with #{@name} for #{@damage} damage!!!"
@@ -45,9 +64,13 @@ ew = EvalWizarrrd.new
 spell = ew.spell("Flaccid Fireball")
 # => #<EvalWizarrrd::Spell:0x007fb2e22b28b0 @name="Flaccid Fireball">
 
+# No target! You missed.
 spell.cast
 # => "Flaccid Fireball Missed!"
 
+
+# Let's try that again; this time, we'll give the spell
+# some damage, and a target.
 better_spell = ew.spell("Meatier, Sturdier Fireballs") do
   target "Matt Otto"  
   damage 42  
--- session_2/eval_wizard_2.rb
+++ session_2/eval_wizard_2.rb
@@ -1,9 +1,11 @@
+# EvalWizarrrrd wants more power, y'all.
 class EvalWizarrrd
   attr_reader :level
   def initialize
     @level = 1
   end
 
+  # Level up. You can't level down.
   def level_up!
     @level += 1
   end
@@ -14,13 +16,18 @@ class EvalWizarrrd
     spell
   end
 
+  # Our custom spell changed.
   def lightning_bolt(tgt)
+    # What the hell is this, Javascript?
     _self = self
     spell("Lighting Bolt!!! ZAPP!!!") do
       target tgt
       damage do
-        rand(111) + ((_self.level * 1.085) * 35)
-        # rand(111)
+        # We want dynamic damage, based on the EvalWizarrrrd#level.
+        # Problem is, `instance_eval` up there has flattened.
+        # That's why we need to stash `self` in `_self`, which is
+        # enclosed by this block.
+        100 + rand(111) * (_self.level * 0.827)
       end
     end
   end
@@ -35,8 +42,12 @@ class EvalWizarrrd
       @target = tgt
     end
 
+    # Make damage a little more dynamic.
+    # If a block is passed, save it as
+    # a Proc; otherwise, make one with
+    # the flat +dmg+ amount
     def damage dmg = nil, &blk
-      @damage = blk || lambda { dmg }
+      @damage = blk || Proc.new { dmg }
     end
 
     def cast
@@ -49,5 +60,18 @@ end
 ew = EvalWizarrrd.new
 # => #<EvalWizarrrd:0x007fb2e3814d70>
 
-# ew.lightning_bolt("Matt Otto").cast
-# NameError: undefined local variable or method `level' for #<EvalWizarrrd::Spell:0x007fdadc018790>
+spell = ew.lightning_bolt("Matt Otto")
+
+# It's different every time!
+3.times { puts spell.cast }
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 137.215 damage!!!
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 162.025 damage!!!
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 160.37099999999998 damage!!!
+
+# What if the wizarrrd levels up?
+ew.level_up!
+3.times { puts spell.cast }
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 230.666 damage!!!
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 219.088 damage!!!
+# Hit Matt Otto with Lighting Bolt!!! ZAPP!!! for 134.73399999999998 damage!!!
+
